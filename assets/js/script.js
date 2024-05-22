015baa74ace9f280151dbc290c3a106662513325
AmanPunia-510 <puniaaman888@gmail.com>
2024-05-22 20:52:01 +0530

    work on accordian

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,37 @@ $(".slider").slick({
   pauseOnHover: true,
   cssEase: "linear",
 });
+
+// accrodian
+
+let accordian_items = document.querySelectorAll(".accordian-box");
+
+accordian_items.forEach(items => {
+    let paragraph = items.querySelector(".accordian-p");
+    let heading = items.querySelector(".accordian-txt-or-svg");
+    let svg = items.querySelector(".accordian-svg");
+
+    heading.style.cursor = "pointer";
+    heading.addEventListener("click", () => {
+
+        accordian_items.forEach(other => {
+            if (other !== items) {
+                let paragraph = other.querySelector(".accordian-p");
+                let svg = other.querySelector(".accordian-svg");
+                paragraph.classList.add("d-none")
+                svg.innerHTML = "+"
+            }
+        });
+
+        if (paragraph.classList.contains("d-none")) {
+            paragraph.classList.remove("d-none")
+            paragraph.classList.add("d-block");
+            svg.innerHTML = "-"
+        }
+        else {
+            paragraph.classList.remove("d-block")
+            paragraph.classList.add("d-none")
+            svg.innerHTML = "+"
+        }
+    })
+})
