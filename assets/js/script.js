6ea148d750794122a7ed00a48bb869d85122bf0b
AmanPunia-510 <puniaaman888@gmail.com>
2024-05-22 13:47:37 +0530

    section two complete now work on section three

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(".slider").slick({
+  arrows: false,
+  dots: false,
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  variableWidth: true,
+  slidesToShow: 6,
+  autoplay: true,
+  autoplaySpeed: 0,
+  speed: 2000,
+  pauseOnHover: true,
+  cssEase: "linear",
+});
