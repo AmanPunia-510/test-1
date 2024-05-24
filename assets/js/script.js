fd0c3f12bbaa2bf2334f978d875845e1605fb2bb
AmanPunia-510 <puniaaman888@gmail.com>
2024-05-24 06:39:48 +0530

    work on roots and features section

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,32 +17,55 @@ $(".slider").slick({
 
 let accordian_items = document.querySelectorAll(".accordian-box");
 
-accordian_items.forEach(items => {
-    let paragraph = items.querySelector(".accordian-p");
-    let heading = items.querySelector(".accordian-txt-or-svg");
-    let svg = items.querySelector(".accordian-svg");
+accordian_items.forEach((items) => {
+  let paragraph = items.querySelector(".accordian-p");
+  let heading = items.querySelector(".accordian-txt-or-svg");
+  let svg = items.querySelector(".accordian-svg");
 
-    heading.style.cursor = "pointer";
-    heading.addEventListener("click", () => {
+  heading.style.cursor = "pointer";
+  heading.addEventListener("click", () => {
+    accordian_items.forEach((other) => {
+      if (other !== items) {
+        let paragraph = other.querySelector(".accordian-p");
+        let svg = other.querySelector(".accordian-svg");
+        paragraph.classList.add("d-none");
+        svg.innerHTML = "+";
+      }
+    });
 
-        accordian_items.forEach(other => {
-            if (other !== items) {
-                let paragraph = other.querySelector(".accordian-p");
-                let svg = other.querySelector(".accordian-svg");
-                paragraph.classList.add("d-none")
-                svg.innerHTML = "+"
-            }
-        });
+    if (paragraph.classList.contains("d-none")) {
+      paragraph.classList.remove("d-none");
+      paragraph.classList.add("d-block");
+      svg.innerHTML = "-";
+    } else {
+      paragraph.classList.remove("d-block");
+      paragraph.classList.add("d-none");
+      svg.innerHTML = "+";
+    }
+  });
+});
 
-        if (paragraph.classList.contains("d-none")) {
-            paragraph.classList.remove("d-none")
-            paragraph.classList.add("d-block");
-            svg.innerHTML = "-"
-        }
-        else {
-            paragraph.classList.remove("d-block")
-            paragraph.classList.add("d-none")
-            svg.innerHTML = "+"
-        }
-    })
-})
+const item = document.querySelector(".navbar-items");
+const menu = document.querySelector(".toggle");
+
+menu.addEventListener("click", () => {
+  item.classList.toggle("show");
+  let contain = document.querySelector("body");
+  let index = document.querySelector(".js-z");
+
+  if (item.classList.contains("show")) {
+    contain.style.position = "fixed";
+    contain.style.top = "0";
+    contain.style.left = "0";
+    contain.style.width = "100%";
+    index.classList.add("z-0");
+    index.classList.remove("z-2");
+  } else {
+    contain.style.position = "unset";
+
+    setTimeout(() => {
+      index.classList.remove("z-0");
+      index.classList.add("z-2");
+    }, 200);
+  }
+});
